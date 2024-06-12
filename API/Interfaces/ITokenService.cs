835e494cf75b850a0de938bf10155891c0a98772
Owais Khan <51928444+owaiskhan3142@users.noreply.github.com>
2024-06-12 20:30:43 +0500

    fixed

--- API/Interfaces/ITokenService.cs
+++ API/Interfaces/ITokenService.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Entities;
 
 namespace API.Interfaces
 {
-
     public interface ITokenService
     {
         string CreateToken(AppUser user);
