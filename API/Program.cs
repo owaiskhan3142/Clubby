2f665ce39832b17730c3dace0f78ccad9de0ec51
Owais Khan <51928444+owaiskhan3142@users.noreply.github.com>
2024-06-15 18:07:49 +0500

    final

--- API/Program.cs
+++ API/Program.cs
@@ -1,38 +1,24 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JWTBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options => )
-
-// void JWTBearerDefaults(AuthenticationOptions options)
-// {
-//     throw new NotImplementedException();
-// }
-
-// void JWTBearerDefaults(AuthenticationOptions options)
-// {
-//     throw new NotImplementedException();
-// }
 
 var app = builder.Build();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 app.Run();
