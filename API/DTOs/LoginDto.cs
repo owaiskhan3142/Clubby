4b638293082fbb304e5b2eeb78f8892b27406a98
Owais Khan <51928444+owaiskhan3142@users.noreply.github.com>
2024-06-15 23:03:31 -0700

    final

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -8,7 +8,6 @@ namespace API.DTOs
     public class LoginDto
     {
         public string Username { get; set; }
-
         public string Password { get; set; }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,18 +1,10 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
-
 
 var app = builder.Build();
 
